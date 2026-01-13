5d9b022e01ea6932d2f877e7d5ec2ef1388beef1
shubhamxcode <shubh.varshneycode@gmail.com>
2026-01-14 01:35:18 +0530

    react revision

---
+++ Reactjs/SPA.js
@@ -0,0 +1,6 @@
+// single page application, the browser load a single html page and update that page without redloading the page
+
+// there is a disadvantage of spa is on refresh data is lost
+
+
+// React uses client-side routing (like React Router) to achieve SPA behavior.
--- javascript/loops/forloop.js
+++ javascript/loops/forloop.js
@@ -12,4 +12,3 @@ for (let i=0; i<countryname.length; i++){
     console.log(countryname[i])
 }
 
-
